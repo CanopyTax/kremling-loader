e0c752c50187175754d95d4b8abc94d55ad8e627
geoctrl <tonylefler@gmail.com>
2018-07-24 11:11:59 -0600

    output a simple id / fix related things

--- test/utils.js
+++ test/utils.js
@@ -1,6 +1,6 @@
 exports.parseEsModuleString = function(str) {
   // make sure this is an es module
-  if (str.indexOf('export default') !== 0) return false;
-  const toParse = `(${str.substring(0, str.length - 1).slice(15).trim()})`;
+  if (str.indexOf('module.exports = ') !== 0) return false;
+  const toParse = `(${str.substring(0, str.length - 1).slice(16).trim()})`;
   return eval(toParse);
 }
