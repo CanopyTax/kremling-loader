b5ee39a462e692e539523ef6069f66962c26fabe
tonylefler <tonylefler@gmail.com>
2019-02-18 03:09:37 -0700

    fix id issue

--- src/postcss-kremling-plugin.js
+++ src/postcss-kremling-plugin.js
@@ -1,15 +1,14 @@
 const postcss = require('postcss');
 const parser = require('postcss-selector-parser');
-const kremId = require('./kremling-id');
 
-module.exports = function(namespace) {
+module.exports = function(id, namespace) {
   function parseSelectors(ruleSelectors, space) {
     return parser(selectors => {
       selectors.each(selector => {
-        const attr = parser.attribute({ attribute: `${namespace}="${kremId.id}"` });
+        const attr = parser.attribute({ attribute: `${namespace}="p${id}"` });
         const combinator = parser.combinator({ value: ' ' });
   
-        if (selector.at(0).type === 'class' || selector.at(0).type == 'id' || space) {
+        if (selector.at(0).type === 'class' || selector.at(0).type === 'id' || space) {
           selector.insertBefore(selector.at(0), attr);
           if (space) selector.insertAfter(selector.at(0), combinator);
         } else {
@@ -20,9 +19,8 @@ module.exports = function(namespace) {
     }).processSync(ruleSelectors, { lossless: false });
   }  
 
-  return postcss.plugin('postcss-kremling-plugin', function () {
+  return postcss.plugin('postcss-kremling-plugin', function() {
     return function (root) {
-      kremId.increment();
       root.walkRules(function (rule) {
         rule.selector = `${parseSelectors(rule.selector, true)},${parseSelectors(rule.selector, false)}`;
         return rule;
